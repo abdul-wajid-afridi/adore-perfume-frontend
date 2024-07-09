5533f0091b940efc477dd5b94f8944cc5701ef31
Abdul Wajid Afridi <wajidafridi8554@gmail.com>
2024-07-09 19:27:37 +0500

    Add <NewArrivals> in Home page
    Resolves:#15

---
+++ src/components/cards/product-card.tsx
@@ -0,0 +1,28 @@
+import { memo } from "react";
+
+type TProductCardProps = {
+  name: string;
+  src: string;
+};
+
+const ProductCard = memo(function ProductCard(props: TProductCardProps) {
+  return (
+    <div className="relative group overflow-hidden ">
+      <img
+        className="group-hover:scale-105 duration-300 rounded-sm w-full h-full "
+        src={props.src}
+        alt={props.name}
+      />
+
+      <p className="absolute bottom-3 left-3 text-white z-50 group-hover:border-b border-slate-700 w-fit opacity-0 group-hover:opacity-100 duration-300">
+        {props.name}
+      </p>
+      <p className="text-xs text-secondary z-50 absolute top-0 right-0 hidden group-hover:flex bg-primary p-1 rounded-full rounded-bl-none animate-pulse">
+        Adore Perfume
+      </p>
+      <span className="absolute inset-0 bg-black/50 hidden group-hover:flex" />
+    </div>
+  );
+});
+
+export default ProductCard;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,12 +2,14 @@ import { memo } from "react";
 import HorizontalScrollCarousel from "../../components/scroll-carousel/horizontal-scroll-carousel";
 import Slider from "./components/slider";
 import OurCollections from "./components/our-collections";
+import NewArrivals from "./components/new-arrivals";
 
 const HomePage = memo(function HomePage() {
   return (
     <div>
       <Slider />
       <HorizontalScrollCarousel />
+      <NewArrivals />
       <OurCollections />
     </div>
   );
