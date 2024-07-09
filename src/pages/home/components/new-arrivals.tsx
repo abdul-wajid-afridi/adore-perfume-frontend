b1b018255f80625af0b75874bc498d57a50f7b67
Abdul Wajid Afridi <wajidafridi8554@gmail.com>
2024-07-09 19:27:37 +0500

    Add <NewArrivals> in Home page
    Resolves:#15

---
+++ src/pages/home/components/new-arrivals.tsx
@@ -0,0 +1,25 @@
+import { memo } from "react";
+import ProductCard from "../../../components/cards/product-card";
+
+const SLIDER_IMAGES = [
+  "/home-slider/spray1.jpg",
+  "/home-slider/spray2.jpg",
+  "/home-slider/spray3.jpg",
+  "/home-slider/spray4.jpg",
+  "/home-slider/spray5.jpg",
+];
+
+const NewArrivals = memo(function NewArrivals() {
+  return (
+    <>
+      <h2 className="my-10">New Arrivals</h2>
+      <div className="grid justify-center gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 px-5 sm:px-10">
+        {SLIDER_IMAGES.map((it) => (
+          <ProductCard name="product name" src={it} />
+        ))}
+      </div>
+    </>
+  );
+});
+
+export default NewArrivals;
