c90443c70ef431da2c2993a05ae46643dc7d34fd
Abdul Wajid Afridi <wajidafridi8554@gmail.com>
2024-07-18 23:21:04 +0500

    Add <WhatsappButton>
    *Resolves: #31

---
+++ src/components/whatsapp-button/index.tsx
@@ -0,0 +1,25 @@
+import { memo } from "react";
+import { Button } from "../ui/button";
+
+const WhatsAppButton = memo(function WhatsApp() {
+  //   add random number for now
+  const number = "03122435456";
+  const phoneNumber = number?.slice(1, 20);
+  const redirectToWhatsApp = () => {
+    const message = "Added random number for now!";
+
+    const whatsappUrl = `https://wa.me/+92${phoneNumber}?text=${encodeURIComponent(
+      message
+    )}`;
+    window.location.href = whatsappUrl;
+  };
+  return (
+    <div
+      className="fixed bottom-20 cursor-pointer right-5 animate-pulse"
+      onClick={redirectToWhatsApp}
+    >
+      <img src="/svgs/whatsap.svg" className="h-8 w-8 rounded-full ml-2" />
+    </div>
+  );
+});
+export default WhatsAppButton;
